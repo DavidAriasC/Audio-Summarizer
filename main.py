77cbaaf43bd391e5646c7667eec2410d59465816
David Arias <dariascu@gmail.com>
2023-03-09 00:44:28 -0600

    v1

---
+++ main.py
@@ -0,0 +1,25 @@
+import openai
+openai.api_key = "YOUR_KEY"
+
+
+def summarize_audio(audio_file):
+    # First, transcribe the audio using Whisper API
+    audio_file= open(audio_file, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+    
+    # Then, use ChatGPT to generate a summary of the transcription
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Please summarize the following text: {transcript}",
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    
+    summary = response['choices'][0]['text']
+    
+    return summary
+
